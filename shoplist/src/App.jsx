61591a10376d7cccbb8f051a287f15d4eecc679c
Clara Andrade <mca9911@outlook.com>
2022-11-12 15:56:43 -0300

    inicio/2

--- shoplist/src/App.jsx
+++ shoplist/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Login from "./pages/Login/Login"
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Login />
       
     </div>
   )
---
+++ shoplist/src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Inspiração from "/src/assets/Inspiração.png";
+import '../Login/Login.css';
+
+
+
+function Login() {
+   
+}
+
+
+export default Login
