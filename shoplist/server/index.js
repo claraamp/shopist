a294f477f0c5f99486ddd618c06fb35edbc32db5
Clara Andrade <mca9911@outlook.com>
2022-11-28 20:14:39 -0300

    inicio back popup não foi

--- shoplist/server/index.js
+++ shoplist/server/index.js
@@ -1,17 +1,38 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 const bodyparser = require('body-parser');
+const mysql = require('mysql2');
+var path = require('path');
+
+
+const connection = mysql.createConnection({
+    host: 'sql10.freesqldatabase.com',
+    user: 'sql10581235',
+    password: '4cXHBAGk7A',
+    database: 'sql10581235'
+  });
+
+
 
 app.use(bodyparser.urlencoded({extended: true}))
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "")
-});
-
-app.get("/inicio", (req, res) => {
-    res.sendFile(__dirname + "../src/pages/Inicio/Inicio")
-});
-
-app.listen(3001, () =>{
+app.listen(5173, () =>{
     console.log("rodando");
 });
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '../src/pages/Home', 'Home.jsx'));
+    
+});
+
+app.post("/", (req, res)=>{
+
+    const nome_lista = req.body.criarLista 
+
+    connection.query("INSERT INTO listas (nome_lista) VALUES (?)", [nome_lista], (err, result)=>{
+        console.log(err);
+    })
+})
+
+
